01c5ca7dc49b6840b1326552c092084c75cdb7fe
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-10-23 22:09:47 +0700

    fix directory for macOS

--- src/main.js
+++ src/main.js
@@ -1,36 +1,46 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import Vuetify from 'vuetify'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify, {
-  iconfont: 'md'
-})
-const homedir = require('os').homedir()+'\\.MinimartData';
-const storage = require('electron-json-storage');
-storage.setDataPath(homedir + '\\Mini-mart-electron-vue-data-json');
-
-var fs = require("fs-extra");
- 
-var source =homedir + '\\Mini-mart-electron-vue-data-json'
-var destination = homedir + '\\Backup\\' +new Date().toDateString()
- 
+  iconfont: "md"
+});
+let homedir = require("os").homedir() + "\\.MinimartData";
+let storage = require("electron-json-storage");
+storage.setDataPath(homedir + "\\Mini-mart-electron-vue-data-json");
+let source = homedir + "\\Mini-mart-electron-vue-data-json";
+let destination = homedir + "\\Backup\\" + new Date().toDateString();
+let fs = require("fs-extra");
+let opsys = process.platform;
+if (opsys == "darwin") {
+  homedir = require("os").homedir() + "/.MinimartData";
+  storage.setDataPath(homedir + "/Mini-mart-electron-vue-data-json");
+  source = homedir + "/Mini-mart-electron-vue-data-json";
+  destination = homedir + "/Backup/" + new Date().toDateString();
+  opsys = "MacOS";
+} else if (opsys == "win32" || opsys == "win64") {
+  opsys = "Windows";
+} else if (opsys == "linux") {
+  opsys = "Linux";
+}
+// console.log(opsys);
 // copy source folder to destination
-fs.copy(source, destination, function (err) {
-    if (err){
-        console.log('An error occured while copying the folder.')
-        return console.error(err)
-    }
-    console.log('Copy completed!')
+fs.copy(source, destination, function(err) {
+  if (err) {
+    console.log("An error occured while copying the folder.");
+    return console.error(err);
+  }
+  console.log("Copy completed!");
 });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
